eeba97551f57486272f04f08a7b20f018e2a092c
Bellamy <bdanbellamy@gmail.com>
2024-02-23 06:01:48 +0200

    [CREATE] styles directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './styles/App.css'
 import DisplayConnections from './components/display-connections'
 
 export default function App() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './lib/client.js'
 
