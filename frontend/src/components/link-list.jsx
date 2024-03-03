f9089b0c3016f09195a473c345f315cc77f51b5c
Bellamy <bdanbellamy@gmail.com>
2024-03-03 21:51:55 +0200

    Realtime updates with GraphQL subscriptions

--- frontend/src/components/link-list.jsx
+++ frontend/src/components/link-list.jsx
@@ -1,10 +1,33 @@
 import { useQuery } from '@apollo/client'
 import Link from "./link"
-import { FEED_QUERY } from '../lib/mutations';
-
+import { FEED_QUERY, NEW_LINKS_SUBSCRIPTION, NEW_VOTES_SUBSCRIPTION } from '../lib/mutations';
 
 export default function LinkList() {
-    const { loading, error, data } = useQuery(FEED_QUERY);
+    const { loading, error, data, subscribeToMore } = useQuery(FEED_QUERY);
+
+    subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+            const newLink = subscriptionData.data.newLink;
+            const exists = prev.feed.links.find(
+                ({ id }) => id === newLink.id
+            );
+            if (exists) return prev;
+
+            return Object.assign({}, prev, {
+                feed: {
+                    links: [newLink, ...prev.feed.links],
+                    count: prev.feed.links.length + 1,
+                    __typename: prev.feed.__typename
+                }
+            });
+        }
+    });
+
+    subscribeToMore({
+        document: NEW_VOTES_SUBSCRIPTION
+    });
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
--- frontend/src/lib/client.js
+++ frontend/src/lib/client.js
@@ -1,11 +1,16 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context'
+import { setContext } from '@apollo/client/link/context';
+import { split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { AUTH_TOKEN } from '../lib/constants';
 
+// HTTP Link
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
 
+// Authorization Link
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem(AUTH_TOKEN);
     return {
@@ -16,7 +21,30 @@ const authLink = setContext((_, { headers }) => {
     }
 })
 
+// Web Socket Link
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000/graphql`,
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN)
+        }
+    }
+})
+
+const link = split(
+    ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+        return (
+            kind === 'OperationDefinition' &&
+            operation === 'subscription'
+        );
+    },
+    wsLink,
+    authLink.concat(httpLink)
+);
+
 export const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache(),
 });
